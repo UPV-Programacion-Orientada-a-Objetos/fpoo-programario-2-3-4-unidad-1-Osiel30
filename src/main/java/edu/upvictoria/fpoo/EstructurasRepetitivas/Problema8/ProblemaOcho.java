6d884239a006b629d1af8c03480e1f0fc5af8333
Osiel30 <2230412@upv.edu.mx>
2024-01-30 22:41:35 -0600

    Se genera codigo, test y metodo de Problema Ocho

---
+++ src/main/java/edu/upvictoria/fpoo/EstructurasRepetitivas/Problema8/ProblemaOcho.java
@@ -0,0 +1,12 @@
+package edu.upvictoria.fpoo.EstructurasRepetitivas.Problema8;
+
+public class ProblemaOcho {
+    public static void main(String[] args) {
+        ImprimirPares();
+    }
+    public static void ImprimirPares() {
+        for (int i = 0; i <= 100; i += 2) {
+            System.out.println(i);
+        }
+    }
+}
