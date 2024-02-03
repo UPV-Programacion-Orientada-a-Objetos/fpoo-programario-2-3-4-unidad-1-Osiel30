81a858624ed31b0bb7a44011d0f29487b1555eee
Osiel30 <2230412@upv.edu.mx>
2024-02-02 22:45:24 -0600

    Se genera codigo del problema 2

--- src/main/java/edu/upvictoria/fpoo/Arreglos/Problema2/Suma.java
+++ src/main/java/edu/upvictoria/fpoo/Arreglos/Problema2/Suma.java
@@ -1,4 +1,18 @@
 package edu.upvictoria.fpoo.Arreglos.Problema2;
-
 public class Suma {
+    public static void sumarArreglos(float[] arregloA, float[] arregloB) {
+        int tamano = arregloA.length;
+        float[] arregloC = new float[tamano];
+
+        for (int i = 0; i < tamano; i++) {
+            arregloC[i] = arregloA[i] + arregloB[i];
+        }
+        mostrarResultado(arregloC);
+    }
+    private static void mostrarResultado(float[] arreglo) {
+        System.out.print("La sumatoria es de: ");
+        for (float elemento : arreglo) {
+            System.out.print(elemento + " ");
+        }
+    }
 }
