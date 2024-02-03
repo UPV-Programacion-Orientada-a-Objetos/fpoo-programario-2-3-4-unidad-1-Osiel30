298cd4979e493e19c565eaac5ef9ec80a5c65ded
Osiel30 <2230412@upv.edu.mx>
2024-02-02 22:07:17 -0600

    Se genera codigo del problema 16

--- src/main/java/edu/upvictoria/fpoo/EstructurasSelectivas/Problema16/ProblemaDieciseis.java
+++ src/main/java/edu/upvictoria/fpoo/EstructurasSelectivas/Problema16/ProblemaDieciseis.java
@@ -1,4 +1,22 @@
 package edu.upvictoria.fpoo.EstructurasSelectivas.Problema16;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 public class ProblemaDieciseis {
+    public static void Pago() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Ingrese las horas trabajadas: ");
+        int horasTrabajadas = Integer.parseInt(reader.readLine());
+        System.out.println("Ingrese el pago por hora: ");
+        double pagoHora = Double.parseDouble(reader.readLine());
+        double sueldoSemanal;
+        if (horasTrabajadas > 40) {
+            int horasExtra = horasTrabajadas - 40;
+            sueldoSemanal = (40 * pagoHora) + (horasExtra * (pagoHora * 2));
+        } else {
+            sueldoSemanal = horasTrabajadas * pagoHora;
+        }
+        System.out.println("El sueldo semanal del trabajador es: $" + sueldoSemanal);
+    }
 }
