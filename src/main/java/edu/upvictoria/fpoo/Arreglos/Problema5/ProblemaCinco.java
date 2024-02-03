2ce4972ac6c278e1f3aa0638ead62afae7d45e22
Osiel30 <2230412@upv.edu.mx>
2024-02-02 23:20:05 -0600

    Se genera codigo del problema 5

--- src/main/java/edu/upvictoria/fpoo/Arreglos/Problema5/ProblemaCinco.java
+++ src/main/java/edu/upvictoria/fpoo/Arreglos/Problema5/ProblemaCinco.java
@@ -1,4 +1,19 @@
 package edu.upvictoria.fpoo.Arreglos.Problema5;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 public class ProblemaCinco {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Ingrese los elementos del vector:");
+        int[] vector = new int[6];
+        for (int i = 0; i < vector.length; i++) {
+            System.out.print("Elemento " + (i + 1) + ": ");
+            vector[i] = Integer.parseInt(br.readLine());
+        }
+        Intercambio.realizarIntercambio(vector);
+
+        br.close();
+    }
 }
