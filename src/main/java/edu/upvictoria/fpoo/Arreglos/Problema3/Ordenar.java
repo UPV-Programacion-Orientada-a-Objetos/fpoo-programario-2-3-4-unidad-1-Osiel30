c2ff1da2070ab86856c0756f3c9952f424819710
Osiel30 <2230412@upv.edu.mx>
2024-02-02 23:17:28 -0600

    Se genera codigo del problema 4

--- src/main/java/edu/upvictoria/fpoo/Arreglos/Problema3/Ordenar.java
+++ src/main/java/edu/upvictoria/fpoo/Arreglos/Problema3/Ordenar.java
@@ -1,4 +1,20 @@
 package edu.upvictoria.fpoo.Arreglos.Problema3;
-
 public class Ordenar {
+    public static void ordenarPorPromedio(String[] nombres, double[] promedios) {
+        int tamano = nombres.length;
+
+        for (int i = 0; i < tamano - 1; i++) {
+            for (int j = 0; j < tamano - 1 - i; j++) {
+                if (promedios[j] < promedios[j + 1]) {
+                    double auxPromedio = promedios[j];
+                    promedios[j] = promedios[j + 1];
+                    promedios[j + 1] = auxPromedio;
+
+                    String auxNombre = nombres[j];
+                    nombres[j] = nombres[j + 1];
+                    nombres[j + 1] = auxNombre;
+                }
+            }
+        }
+    }
 }
--- src/main/java/edu/upvictoria/fpoo/Arreglos/Problema4/ProblemaCuatro.java
+++ src/main/java/edu/upvictoria/fpoo/Arreglos/Problema4/ProblemaCuatro.java
@@ -1,4 +1,26 @@
 package edu.upvictoria.fpoo.Arreglos.Problema4;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class ProblemaCuatro {
+    public static void main(String[] args) throws IOException {
+        int[] A = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] B = new int[10];
+
+        System.out.println("Ingrese los elementos del arreglo B:");
+        ingresarElementos(B);
+        Productos.run(A, B);
+    }
+
+    private static void ingresarElementos(int[] arreglo) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        for (int i = 0; i < arreglo.length; i++) {
+            System.out.println("Ingrese el número " + (i + 1) + ":");
+            arreglo[i] = Integer.parseInt(br.readLine());
+        }
+
+        br.close();
+    }
 }
