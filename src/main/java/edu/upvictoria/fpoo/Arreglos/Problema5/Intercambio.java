2ef7738a44b685db6f6197e7f5a0325b8918bad6
Osiel30 <2230412@upv.edu.mx>
2024-02-02 23:19:20 -0600

    Se genera codigo del problema 5

--- src/main/java/edu/upvictoria/fpoo/Arreglos/Problema5/Intercambio.java
+++ src/main/java/edu/upvictoria/fpoo/Arreglos/Problema5/Intercambio.java
@@ -1,4 +1,16 @@
 package edu.upvictoria.fpoo.Arreglos.Problema5;
-
 public class Intercambio {
+    public static void realizarIntercambio(int[] vector) {
+        System.out.println("Vector Intercambiado:");
+
+        for (int i = 0; i < vector.length / 2; i++) {
+            int temp = vector[i];
+            vector[i] = vector[vector.length - 1 - i];
+            vector[vector.length - 1 - i] = temp;
+        }
+
+        for (int i = 0; i < vector.length; i++) {
+            System.out.println("Elemento " + (i + 1) + ": " + vector[i]);
+        }
+    }
 }
