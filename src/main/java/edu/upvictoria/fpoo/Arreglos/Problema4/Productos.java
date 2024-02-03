967760f0b5a412edfe1fa6512565aa89f637e26c
Osiel30 <2230412@upv.edu.mx>
2024-02-02 23:18:03 -0600

    Se genera codigo del problema 4

--- src/main/java/edu/upvictoria/fpoo/Arreglos/Problema4/Productos.java
+++ src/main/java/edu/upvictoria/fpoo/Arreglos/Problema4/Productos.java
@@ -1,4 +1,23 @@
 package edu.upvictoria.fpoo.Arreglos.Problema4;
-
 public class Productos {
+    public static void run(int[] A, int[] B) {
+        int[] C = new int[10];
+
+        for (int i = 0; i < A.length; i++) {
+            if (A[i] == B[i]) {
+                C[i] = A[i];
+            } else if (B[i] > A[i]) {
+                int diferencia = B[i] - A[i];
+                C[i] = 2 * diferencia;
+            } else {
+                C[i] = B[i];
+            }
+        }
+
+        System.out.print("Vector C: ");
+        for (int i = 0; i < C.length; i++) {
+            System.out.print(C[i] + " ");
+        }
+        System.out.println();
+    }
 }
