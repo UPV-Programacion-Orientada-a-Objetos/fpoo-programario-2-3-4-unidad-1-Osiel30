721f1204f16bbb2768d1a311bd459d495692a9e3
Osiel30 <2230412@upv.edu.mx>
2024-02-02 22:44:59 -0600

    Se genera codigo del problema 2

--- src/main/java/edu/upvictoria/fpoo/Arreglos/Problema1/Suma.java
+++ src/main/java/edu/upvictoria/fpoo/Arreglos/Problema1/Suma.java
@@ -1,4 +1,12 @@
 package edu.upvictoria.fpoo.Arreglos.Problema1;
-
 public class Suma {
+    public static void calcularSuma(double[] numeros) {
+        double suma = 0;
+
+        for (double numero : numeros) {
+            suma += numero;
+        }
+
+        System.out.println("La suma es: " + suma);
+    }
 }
--- src/main/java/edu/upvictoria/fpoo/Arreglos/Problema2/ProblemaDos.java
+++ src/main/java/edu/upvictoria/fpoo/Arreglos/Problema2/ProblemaDos.java
@@ -1,4 +1,33 @@
 package edu.upvictoria.fpoo.Arreglos.Problema2;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class ProblemaDos {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("¿De qué tamaño quiere los arreglos?");
+        int tamanoArreglo = Integer.parseInt(br.readLine());
+
+        float[] arregloA = new float[tamanoArreglo];
+        float[] arregloB = new float[tamanoArreglo];
+
+        System.out.println("Ingrese los elementos del primer arreglo (A):");
+        ingresarElementos(arregloA, br);
+
+        System.out.println("Ingrese los elementos del segundo arreglo (B):");
+        ingresarElementos(arregloB, br);
+
+        Suma.sumarArreglos(arregloA, arregloB);
+
+        br.close();
+    }
+
+    private static void ingresarElementos(float[] arreglo, BufferedReader br) throws IOException {
+        for (int i = 0; i < arreglo.length; i++) {
+            System.out.println("Ingrese el número " + (i + 1) + ":");
+            arreglo[i] = Float.parseFloat(br.readLine());
+        }
+    }
 }
