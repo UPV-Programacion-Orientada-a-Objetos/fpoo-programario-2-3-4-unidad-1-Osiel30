b56e9b5ce0c488cb40ab5b7cfec0ed813c3225ba
Osiel30 <2230412@upv.edu.mx>
2024-02-02 22:09:03 -0600

    Se genera codigo del problema 18

--- src/main/java/edu/upvictoria/fpoo/EstructurasSelectivas/Problema18/ProblemaDieciocho.java
+++ src/main/java/edu/upvictoria/fpoo/EstructurasSelectivas/Problema18/ProblemaDieciocho.java
@@ -1,4 +1,23 @@
 package edu.upvictoria.fpoo.EstructurasSelectivas.Problema18;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class ProblemaDieciocho {
+    public static void HorasdeEstacionamiento() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Ingrese la cantidad de horas de estacionamiento: ");
+        int horas = Integer.parseInt(reader.readLine());
+        double costo;
+        if (horas <= 2) {
+            costo = horas * 5.00;
+        } else if (horas <= 5) {
+            costo = 2 * 5.00 + (horas - 2) * 4.00;
+        } else if (horas <= 10) {
+            costo = 2 * 5.00 + 3 * 4.00 + (horas - 5) * 3.00;
+        } else {
+            costo = 2 * 5.00 + 3 * 4.00 + 5 * 3.00 + (horas - 10) * 2.00;
+        }
+        System.out.println("El costo del estacionamiento es: $" + costo);
+    }
 }
